69f70c7134186fc87a1254b650780bb66a833478
jr1234567890 <kraken@thereuters.com>
2020-12-06 15:56:08 -0500

    Clerical fixes 12/6 20

--- rex2020.py
+++ rex2020.py
@@ -54,10 +54,12 @@ else:
 # # Set up servo unit limits
 # # These are determined by testing to be the extents of the servos before they hit a hard stop.
 # # They may not be symmetric, but that will be adjusted with the center values below.
+# Note that there are also min/max limits in the arduino code
 x_min = conf["sx_min"]  # left`
 x_max = conf["sx_max"]  # right
 y_min = conf["sy_min"]  # down
 y_max = conf["sy_max"]  # up
+
 max_servo_slew= conf["max_servo_slew"]
 
 # set servo center and scale
@@ -758,7 +760,10 @@ while(True):  # replace with some kind of test to see if WebcamStream is still a
     if(pointx==90):
         pointx=pointx+osc_value
 
-    # write the command values to the arduino.
+
+    ##################   Send to Arduino  #############################
+
+    # skip this if the arduino skip flag is set
     if(skipflag==0):
         commandecho=myRexCommand.update(pointx, pointy, mouth_pos, eye_cmd,tilt_servo,max_servo_slew)    
         #print(tilt_servo) 
@@ -801,7 +806,7 @@ while(True):  # replace with some kind of test to see if WebcamStream is still a
     #cv2.putText(frame, text, (10, 30),
     #        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 255), 2)
 
-    ########################       DRAW the detectinos, and the selected object  ################
+    ########################       DRAW the detections, and the selected object  ################
 
     for (objectID, centroid) in objects.items():
         cx=xlist[objectID]
@@ -931,7 +936,7 @@ if skipflag==0:
     #     time.sleep(2/step)
 
     #10/25/20 - added slew function in arduino, and we don't need the above slew anymore
-    commandecho=myRexCommand.update(90, 90, 90, 0, 90)
+    commandecho=myRexCommand.update(90, 90, 90, 0, 90,max_servo_slew)
 
     
 
